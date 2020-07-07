23e277db01dc0fbfe8f8576706e57849a51de684
geekbrains <ashulyaev93@mail.ru>
2020-07-07 18:30:11 +0400

    Change homework after watch lection.

--- src/homework5_Java2/Main.java
+++ src/homework5_Java2/Main.java
@@ -1,8 +1,10 @@
 package homework5_Java2;
 
+import java.lang.*;
+
 //          Отличие первого метода от второго:
-//          В первом методе просто бежим по массиву и вычисляем значение.
-//          Второй метод разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
+//          Первый просто бежит по массиву и вычисляет значения.
+//          Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
 
 //          По замерам времени:
 //          Для первого метода надо считать время только на цикл расчета:
@@ -13,16 +15,16 @@ package homework5_Java2;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-//          Вызов метода со стандартным решением;
-        Main.oneArray();
+//          Вызов первого задания со стандартным решением;
+        Main.doMethodOne();
 
-//          Вызов многопоточного класса;
-        new Thread(new MyRunnableClassArray()).start();
+//          Вызов задания с многопоточностью;
+        Main.doMethodTwo();
     }
 
-    public static void oneArray(){
+    public static void doMethodOne(){
 
 //          1) Создаём одномерный длинный массив:
         final int size = 10000000;
@@ -41,59 +43,57 @@ public class Main {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
-//          5) Проверяем время окончания метода:
-        System.currentTimeMillis();
-
-//          6) В консоль выводим время работы:
-        System.out.println(System.currentTimeMillis() - startTime + ": расчёт для первого метода");
+//          5) В консоль выводим время работы:
+        System.out.println(System.currentTimeMillis() - startTime + ": расчёт времени для первого метода");
     }
 
-    static class MyRunnableClassArray implements Runnable {
+    public static void doMethodTwo(){
+        final int size = 10000000;
+        final int newSize = size / 2;
+        float[] arr = new float[size];
+        float[] arr1 = new float[size];
+        float[] arr2 = new float[size];
 
-        @Override
-        public void run() {
-
-            final int size = 10000000;
-
-//          1) Создаём одномерный длинный массив и массивы для разбивания и склеёки;
-            final int newSize = size / 2;
-            float[] arr = new float[size];
-            float[] arr1 = new float[size];
-            float[] arr2 = new float[size];
-
-//          2) Заполняем массив arr единицами;
-            for (int i = 0; i < newSize; ++i) {
-                arr[i] = 1;
-            }
-
-//          3) Засекаем время выполнения;
-            long startTime = System.currentTimeMillis();
-
-//          4) Делим один массив на два;
-            System.arraycopy(arr, 0, arr1, 0, newSize);
-            System.arraycopy(arr, newSize, arr2, 0, newSize);
-
-//          5) Проходим по двум массивам и для каждой ячейки считаем новое значение по формуле;
-            for (int i = 0; i < size; i++) {
-                arr1[i] = (long) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-            for (int i = 0; i < size; i++) {
-                arr2[i] = (long) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-
-//          6) Обратная склейка;
-            System.arraycopy(arr1, 0, arr, 0, newSize);
-            System.arraycopy(arr2, 0, arr, newSize, newSize);
-
-//          Примечание:
-//          System.arraycopy() – копирует данные из одного массива в другой;
-//          System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение, откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем);
-
-//          7) Проверяем время окончания метода;
-            System.currentTimeMillis();
-
-//          8) В консоль выводим время работы;
-            System.out.println(System.currentTimeMillis() - startTime + ": расчёт для второго метода");
+        for (int i = 0; i < size; ++i) {
+            arr[i] = 1;
         }
+
+        long startTime = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, arr1, 0, newSize);
+        System.arraycopy(arr, newSize, arr2, 0, newSize);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i < arr1.length; i++) {
+                    arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i < arr2.length; i++) {
+                    arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.arraycopy(arr1, 0, arr, 0, newSize);
+        System.arraycopy(arr2, 0, arr, newSize, newSize);
+
+        System.out.println(System.currentTimeMillis() - startTime + ": расчёт времени для второго метода, с многопоточностью");
     }
 }
